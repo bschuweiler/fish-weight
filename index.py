9c2a8850979918221409fba71ee4b3f318cf6f77
Brian Schuweiler <bschuweiler@ShoeBookPro.local>
2018-09-19 21:57:53 -0500

    testing delivery

--- index.py
+++ index.py
@@ -4,9 +4,11 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Stuff',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data),
+        'headers': {'Content-Type': 'application/json'}
+    }
