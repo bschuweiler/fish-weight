0a4dbb440d7d71470015f5984200fa485a41913b
Brian Schuweiler <bschuweiler@ShoeBookPro.local>
2018-09-20 18:56:35 -0500

    exception handling

--- index.py
+++ index.py
@@ -4,21 +4,32 @@ from fishweight import fishweight
 
 
 def handler(event, context):
-    if (not event or
-            not event['pathParameters'] or
-            not (len(event['pathParameters']) is 2) or
-            not event['pathParameters']['species'] or
-            not event['pathParameters']['length']):
-        raise ValueError(
-            "Missing one or both required parameters ('species' and 'length')")
+    try:
+        if (not event or
+                not event['pathParameters'] or
+                not (len(event['pathParameters']) is 2) or
+                not event['pathParameters']['species'] or
+                not event['pathParameters']['length']):
+            raise ValueError(
+                "Missing one or both required parameters"
+                "('species' and 'length')")
 
-    species = event['pathParameters']['species']
-    length = event['pathParameters']['length']
+        species = event['pathParameters']['species']
+        length = event['pathParameters']['length']
 
-    response = fishweight.lengthToWeight(species, length)
+        response = fishweight.lengthToWeight(species, length)
 
-    return {
-        'statusCode': 200,
-        'body': response,
-        'headers': {'Content-Type': 'application/json'}
-    }
+        return {
+            'statusCode': 200,
+            'body': response,
+            'headers': {'Content-Type': 'application/json'}
+        }
+    except Exception as e:
+        response = json.dumps({
+            'Error': str(e)
+        })
+        return {
+            'statusCode': 500,
+            'body': response,
+            'headers': {'Content-Type': 'application/json'}
+        }
--- tests/test_handler.py
+++ tests/test_handler.py
@@ -4,7 +4,7 @@ import index
 
 class TestHandlerCase(unittest.TestCase):
 
-    def test_response(self):
+    def test_response_valid_input(self):
         event = {
             'pathParameters': {
                 'species': 'walleye',
@@ -15,6 +15,18 @@ class TestHandlerCase(unittest.TestCase):
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
         self.assertIn('walleye', result['body'])
+
+    def test_response_invalid_input(self):
+        event = {
+            'pathParameters': {
+                'species': 'Walleye',
+                'length': '23.75'
+            }
+        }
+        result = index.handler(event, None)
+        self.assertEqual(result['statusCode'], 500)
+        self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        self.assertIn('Error', result['body'])
 
 
 if __name__ == '__main__':
